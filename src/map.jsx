530cd02d9e01d1afaa89bb9f8bae7758b22bfefa
ollysamm <125161846+ollysamm@users.noreply.github.com>
2023-04-20 17:55:09 -0600

    Cosmetic changes

--- src/map.jsx
+++ src/map.jsx
@@ -9,12 +9,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85e
 
 const Map = () => {
     const mapContainer = useRef(null);
-    const [lng, setLng] = useState(-114.0571411);
-    const [lat, setLat] = useState(51.0453809);
-    const [zoom, setZoom] = useState(10);
+    const [lng, setLng] = useState(-114.0719);
+    const [lat, setLat] = useState(51.0447);
+    const [zoom, setZoom] = useState(11.5);
     const [title, setTitle] = useState("Park n' Bike");
     const geoCoder = new MapboxGeocoder({
         accessToken: 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g',
+        placeholder: 'Home',
         mapboxgl: mapboxgl,
         marker: {
             color: "orange",
@@ -23,6 +24,7 @@ const Map = () => {
     });
     const geoCoderTwo = new MapboxGeocoder({
         accessToken: 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g',
+        placeholder: 'Work',
         mapboxgl: mapboxgl,
         marker: {
             color: "orange",
@@ -48,7 +50,7 @@ searchTwo.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85
         // if (map.current) return; // initialize map only once
         const map = new mapboxgl.Map({
             container: mapContainer.current,
-            style: "mapbox://styles/mapbox/streets-v12",
+            style: "mapbox://styles/ollysamm/clgmqv6wz001f01pl6kx5gu1y",
             //style: "https://studio.mapbox.com/tilesets/prabhsagoo.clgda45tl00mo2io1woyd8q34-9m1pw",
             center: [lng, lat],
             zoom: zoom,
@@ -83,7 +85,7 @@ searchTwo.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85
     return (
         <div className="data">
             {" "}
-            <h1>{title}</h1>
+            {/* <h1>{title}</h1> */}
             <div ref={mapContainer} className="map-container" />
             <div>
                 <form>
