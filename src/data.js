01fb7f5161d8821d9e82635c300c0168c97237f9
ollysamm <125161846+ollysamm@users.noreply.github.com>
2023-04-20 20:47:15 -0600

    Added in Bicycle Parking data

--- src/data.js
+++ src/data.js
@@ -1,26 +1,123 @@
-export const dataGeo = {
+export const bikeParkingLocations = {
   "type": "FeatureCollection",
   "features": [
     {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-77.032, 38.913]
+      "type":"Feature",
+      "geometry":{
+        "type":"Point",
+        "coordinates":[-114.063806915748,51.048597749212]
       },
-      "properties": {
-        "title": "Mapbox",
-        "description": "Washington, D.C."
+      "properties":{
+        "location_state":null,
+        "location_zip":null,
+        "name":"James Short Parkade",
+        "latitude":"51.0485977492119",
+        "outdoor_stalls":"0",
+        "location_address":null,
+        "location_city":null,
+        "longitude":"-114.063806915748",
+        "web":"https://www.calgaryparking.com/findparking/bicycle",
+        "address":"112 5 Ave SW",
+        "indoor_stalls":"44"
       }
     },
     {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-122.414, 37.776]
+      "type":"Feature",
+      "geometry":{
+        "type":"Point",
+        "coordinates":[-114.068188205223,51.04449658162]
       },
-      "properties": {
-        "title": "Mapbox",
-        "description": "San Francisco, California"
+      "properties":{
+        "location_state":null,
+        "location_zip":null,
+        "name":"City Centre Parkade",
+        "latitude":"51.0444965816196",
+        "outdoor_stalls":"0",
+        "location_address":null,
+        "location_city":null,
+        "longitude":"-114.068188205223",
+        "web":"https://www.calgaryparking.com/findparking/bicycle",
+        "address":"221 9 Ave SW",
+        "indoor_stalls":"104"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":{
+        "type":"Point",
+        "coordinates":[-114.076856205783,51.049066981974]
+      },
+      "properties":{
+        "location_state":null,
+        "location_zip":null,
+        "name":"McDougall Parkade",
+        "latitude":"51.0490669819744",
+        "outdoor_stalls":"0",
+        "location_address":null,
+        "location_city":null,
+        "longitude":"-114.076856205783",
+        "web":"https://www.calgaryparking.com/findparking/bicycle",
+        "address":"720 5 Ave SW",
+        "indoor_stalls":"16"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":{
+        "type":"Point",
+        "coordinates":[-114.074468229421,51.050420156598]
+      },
+      "properties":{
+        "location_state":null,
+        "location_zip":null,
+        "name":"Lot 72",
+        "latitude":"51.0504201565981",
+        "outdoor_stalls":"0",
+        "location_address":null,
+        "location_city":null,
+        "longitude":"-114.074468229421",
+        "web":"https://www.calgaryparking.com/findparking/bicycle",
+        "address":"615 3 Ave SW",
+        "indoor_stalls":"12"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":{
+        "type":"Point",
+        "coordinates":[-114.074951853261,51.045169955783]
+      },
+      "properties":{
+        "location_state":null,
+        "location_zip":null,
+        "name":"Centennial Parkade",
+        "latitude":"51.0451699557825",
+        "outdoor_stalls":"22",
+        "location_address":null,
+        "location_city":null,
+        "longitude":"-114.074951853261",
+        "web":"https://www.calgaryparking.com/findparking/bicycle",
+        "address":"608 9 Ave SW","indoor_stalls":"27"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry":{
+        "type":"Point",
+        "coordinates":[-114.061333138545,51.04596591753]
+      },
+      "properties":{
+        "location_state":null,
+        "location_zip":null,
+        "name":"Convention Centre Parkade",
+        "latitude":"51.04596591752975",
+        "outdoor_stalls":"0",
+        "location_address":null,
+        "location_city":null,
+        "longitude":"-114.06133313854542",
+        "web":"https://www.calgaryparking.com/findparking/bicycle",
+        "address":"727 1 St SE",
+        "indoor_stalls":"8"
       }
     }
   ]
--- src/map.jsx
+++ src/map.jsx
@@ -4,10 +4,11 @@ import mapboxgl from "mapbox-gl"; // eslint-disable-line import/no-webpack-loade
 import { AddressAutofill } from "@mapbox/search-js-react";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import Geolocation from "@mapbox/mapbox-gl-geocoder/lib/geolocation";
-import { dataGeo } from "./data.js";
+import { bikeParkingLocations } from "./data.js";
 mapboxgl.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g';
 
 const Map = () => {
+
     const mapContainer = useRef(null);
     const [lng, setLng] = useState(-114.0719);
     const [lat, setLat] = useState(51.0447);
@@ -19,68 +20,58 @@ const Map = () => {
         mapboxgl: mapboxgl,
         marker: {
             color: "orange",
-
         }
     });
+
     const geoCoderTwo = new MapboxGeocoder({
         accessToken: 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g',
         placeholder: 'Work',
         mapboxgl: mapboxgl,
         marker: {
             color: "orange",
-
         }
     });
-    // "This puts the name, long and lat above the map"
+
     useEffect(() => {
         geoCoder.on("result", function (results) {
             console.log(results.result.place_name);
-            // setTitle(results.result.place_name);
-            // setLng(results.result.center[0]);
-            // setLat(results.result.center[1]);
-            //   setZoom(13);
         });
 
-        const search = new MapboxSearchBox();
-search.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g';
+    const search = new MapboxSearchBox();
+        search.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g';
 
-const searchTwo = new MapboxSearchBox();
-searchTwo.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g';
+    const searchTwo = new MapboxSearchBox();
+        searchTwo.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85eHppc3F0eiJ9.HQqrdoz9EbIevdT2-KdY5g';
 
-        // if (map.current) return; // initialize map only once
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: "mapbox://styles/ollysamm/clgmqv6wz001f01pl6kx5gu1y",
-            //style: "https://studio.mapbox.com/tilesets/prabhsagoo.clgda45tl00mo2io1woyd8q34-9m1pw",
-            center: [lng, lat],
-            zoom: zoom,
-        })
-        .addControl(geoCoder)
-        .addControl(geoCoderTwo)
-        
-            // .addControl(
-            //     new mapboxgl.GeolocateControl({
-            //         positionOptions: {
-            //             enableHighAccuracy: true,
-            //         },
-            //         // When active the map will receive updates to the device's location as it changes.
-            //         trackUserLocation: true,
-            //         // Draw an arrow next to the location dot to indicate which direction the device is heading.
-            //         showUserHeading: true,
-            //         showUserLocation: true,
-            //     })
-            // );
-        for (let i = 0; i < dataGeo.features.length; i++) {
-            new mapboxgl.Marker({
-                color: "#FFFFFF"
-            }).setLngLat([dataGeo.features[i].geometry.coordinates[0], dataGeo.features[i].geometry.coordinates[1]]).addTo(map);
-            console.log(dataGeo.features[i].geometry.coordinates);
-
-        }
+    const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/ollysamm/clgmqv6wz001f01pl6kx5gu1y",
+        center: [lng, lat],
+        zoom: zoom,
+    })
+    .addControl(geoCoder)
+    .addControl(geoCoderTwo)
+    
+        // .addControl(
+        //     new mapboxgl.GeolocateControl({
+        //         positionOptions: {
+        //             enableHighAccuracy: true,
+        //         },
+        //         // When active the map will receive updates to the device's location as it changes.
+        //         trackUserLocation: true,
+        //         // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        //         showUserHeading: true,
+        //         showUserLocation: true,
+        //     })
+        // );
+    for (let i = 0; i < bikeParkingLocations.features.length; i++) {
+        new mapboxgl.Marker({
+            color: "#FFFFFF"
+        }).setLngLat([bikeParkingLocations.features[i].geometry.coordinates[0], bikeParkingLocations.features[i].geometry.coordinates[1]]).addTo(map);
+        console.log(bikeParkingLocations.features[i].geometry.coordinates);
+    }
 
     }, []);
-
-
 
     return (
         <div className="data">
@@ -95,9 +86,10 @@ searchTwo.accessToken = 'pk.eyJ1IjoibHVrZTY2NnoiLCJhIjoiY2xnZmppd2s2MDJlNTNsbW85
         </div>
     );
 };
-function select() {
-    console.log("first");
-}
+
+// function select() {
+//     console.log("first");
+// }
 
 
 export default Map;
