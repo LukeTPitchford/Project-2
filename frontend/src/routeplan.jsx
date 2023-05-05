f85f2507aeb9e8d7705f05f0074c7cf66cdba551
ollysamm <125161846+ollysamm@users.noreply.github.com>
2023-05-05 10:33:51 -0600

    fixed ticker position

--- frontend/src/routeplan.jsx
+++ frontend/src/routeplan.jsx
@@ -20,6 +20,7 @@ import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import zIndex from "@mui/material/styles/zIndex.js";
 
 export default function RoutePlan() {
 
--- frontend/src/ticker/ticker.jsx
+++ frontend/src/ticker/ticker.jsx
@@ -6,23 +6,23 @@ import money from "./gifs/money.gif";
 import weather from "./gifs/weather.gif";
 import factory from "./gifs/factory.gif";
 import "./Ticker.css";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Ticker() {
-  // const [aqi, setAqi] = useState(null);
-  // const [weatherData, setWeather] = useState(null);
+  const [aqi, setAqi] = useState(null);
+  const [weatherData, setWeather] = useState(null);
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const response = await fetch("/api/weather/51.049999/-114.066666");
-  //     const data = await response.json();
-  //     setWeather(data);
-  //     const response2 = await fetch("/api/airquality/51.049999/-114.066666");
-  //     const data2 = await response2.json();
-  //     setAqi(data2);
-  //   };
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("/api/weather/51.049999/-114.066666");
+      const data = await response.json();
+      setWeather(data);
+      const response2 = await fetch("/api/airquality/51.049999/-114.066666");
+      const data2 = await response2.json();
+      setAqi(data2);
+    };
+    getData();
+  }, []);
 
   return (
     <>
@@ -30,7 +30,7 @@ function Ticker() {
         <div className="hwrap">
           <div className="hmove">
             {" "}
-            {/* {weatherData != null && aqi != null ? ( */}
+            {weatherData != null && aqi != null ? (
               <>
                 <div className="hitem">
                   <a href="ticker.html">
@@ -48,13 +48,13 @@ function Ticker() {
                   <a href="ticker.html">
                     <img src={carbon} height="60px" width="100px"></img>
                   </a>
-                  {/* <div>Current co level is: {aqi.list[0].components.co}</div> */}
+                  <div>Current co level is: {aqi.list[0].components.co}</div>
                 </div>
                 <div className="hitem">
                   <a href="ticker.html">
                     <img src={odometer} height="60px" width="100px"></img>
                   </a>
-                  <div>KMS Travelled:</div>
+                  <div>KMS Cycled: 5km</div>
                 </div>
                 <div className="hitem">
                   <a href="ticker.html">
@@ -66,13 +66,13 @@ function Ticker() {
                   <a href="ticker.html">
                     <img src={weather} height="60px" width="100px"></img>
                   </a>
-                  {/* <div>current temperature is :{weatherData.main.temp}</div> */}
+                  <div>current temperature is :{weatherData.main.temp}</div>
                 </div>
                 <div className="hitem">
                   <a href="ticker.html">
                     <img src={factory} height="60px" width="100px"></img>
                   </a>
-                  {/* <div>Current air quality is :{aqi.list[0].main.aqi}</div> */}
+                  <div>Current air quality is :{aqi.list[0].main.aqi}</div>
                 </div>
               </>
             ) : (
